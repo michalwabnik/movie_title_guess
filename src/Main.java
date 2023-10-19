0ee05f5be28f10b4b323badd127c4f24d000acc8
michalwabnik <michal-w@hotmail.com>
2023-10-19 12:39:59 +0200

    Added welcome text to start game

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args){
         GetMovieTitle oMovieTitle = new GetMovieTitle();
         String movieTitle = oMovieTitle.movieTitle();
+        System.out.println("Please try to guess below movie title by adding letters");
         Game game = new Game();
+        String convertedMovieTitle = new Game().displayConvertedMovieTitle("", movieTitle);
+        System.out.println(convertedMovieTitle);
         game.gameLogic(movieTitle);
     }
 }
